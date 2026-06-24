e3dd894007d3bb82f0b992ac0d1d600542550193
Luke <luke.nelissen04@gmail.com>
2026-06-24 22:47:06 +0200

    Add lagging and rolled environmental variables to df

--- src/features/engineering.py
+++ src/features/engineering.py
@@ -1,25 +1,45 @@
 import numpy as np
 import pandas as pd
+# from pandas.api.types import is_float_dtype
 
 
-def add_rolling_statistics(df, periods):
+def add_rolling_statistics(df, columns, periods):
     df = df.sort_values("Timestamp")
-    df_features = pd.DataFrame()
-    df_features["Timestamp"] = df["Timestamp"].copy()
+    df_roll = pd.DataFrame(df["Timestamp"].copy())
 
-    columns = df.columns
+    # columns = df.columns
 
     for c in columns:
         if pd.api.types.is_float_dtype(df[c]):
             for window in periods:
-                df_features[f"{c}_{window}D_mean"] = df[c].rolling(window=window).mean()
-                df_features[f"{c}_{window}D_std"] = df[c].rolling(window=window).std()
-                df_features[f"{c}_{window}D_sum"] = df[c].rolling(window=window).sum()
-                df_features[f"{c}_{window}D_median"] = (
-                    df[c].rolling(window=window).median()
-                )
+                df_roll[f"{c}_{window}D_mean"] = df[c].rolling(window=window).mean()
+                df_roll[f"{c}_{window}D_std"] = df[c].rolling(window=window).std()
+                df_roll[f"{c}_{window}D_sum"] = df[c].rolling(window=window).sum()
+                df_roll[f"{c}_{window}D_median"] = df[c].rolling(window=window).median()
 
-    return df_features
+    return df_roll
+
+
+def roll_cols(df, columns, periods):
+    df = df.sort_values("Timestamp")
+    features = {}
+
+    for c in columns:
+        if pd.api.types.is_float_dtype(df[c]):
+            s = df[c]
+
+            for window in periods:
+                roll = s.rolling(window=window, min_periods=window)
+
+                features[f"{c}_{window}D_mean"] = roll.mean()
+                # features[f"{c}_{window}D_std"] = roll.std()
+                features[f"{c}_{window}D_sum"] = roll.sum()
+            # features[f"{c}_{window}D_median"] = roll.median()
+
+    df_roll = pd.concat(
+        [df[["Timestamp"]], pd.DataFrame(features, index=df.index)], axis=1
+    )
+    return df_roll
 
 
 def lag_col(df, columns, periods):

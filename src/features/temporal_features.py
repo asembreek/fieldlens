fff68b0a9504dfac7adac3b519e33de05723473b
Luke <luke.nelissen04@gmail.com>
2026-07-10 23:21:32 +0200

    Implement cyclic encoding. Luckily not too hard.

--- src/features/temporal_features.py
+++ src/features/temporal_features.py
@@ -1,23 +1,45 @@
 import numpy as np
-import pandas as pd
-from models import NDVIClimatologyGAM as shift
 from data import utils
+from models import NDVIClimatologyGAM as shifter
 
 
 class TemporalFeatures:
-    def __init__(self, cyclic_doy=True):
+    def __init__(self, start_of_season, cyclic_doy=False):
         self.cyclic_doy = cyclic_doy
+        self.start_of_season = start_of_season
 
     def fit(self, X):
         return self
 
     def transform(self, X):
-        pass
+        df = X.copy()
+        if self.cyclic_doy:
+            required = {"doy", "Timestamp"}
+            missing = required - set(X.columns)
+            if len(missing) == 2:
+                raise ValueError(
+                    "Cyclic encoding requires one of {required} as columns."
+                )
+
+            missing_date = False
+
+            if "doy" in missing:
+                df = utils.add_date_info(df)
+                missing_date = True
+
+            df["shifted_doy"] = shifter.shift_doy(df["doy"], self.start_of_season)
+
+            df = self._add_cyclic_doy(df)
+            if missing_date:
+                df = df.drop(["doy", "year"], axis=1)
+
+            df = df.drop("shifted_doy", axis=1)
+        return df
 
     def fit_transform(self, X):
-        return self
+        return self.transform(X)
 
     def _add_cyclic_doy(self, X):
-        X["DOY_sin"] = np.sin(2 * np.pi * X["DOY"] / 365.25)
-        X["DOY_cos"] = np.cos(2 * np.pi * X["DOY"] / 365.25)
+        X["doy_sin"] = np.sin(2 * np.pi * X["shifted_doy"] / 365.25)
+        X["doy_cos"] = np.cos(2 * np.pi * X["shifted_doy"] / 365.25)
         return X
